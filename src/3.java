df1432f990f25334c6cd2bb4f2a004c0ef668ae6
hala9gwcser <hala9gwcser@gmx.com>
2025-03-07 13:19:18 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt((10 - 5) + 1) + 5;
+        System.out.println("Random number: " + randomNumber);
+    }
+}
