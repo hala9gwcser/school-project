373714413b82094b7f2e81459075e9527ea3935f
hala9gwcser <hala9gwcser@gmx.com>
2025-03-08 18:24:54 +0000

    Add 4.java

---
+++ src/4.java
@@ -0,0 +1,7 @@
+public class RandomJavaCode {
+  public static void main(String[] args) {
+    // Generate random code here
+    int x = (int)(Math.random() * 10);
+    System.out.println("The generated number is: " + x);
+  }
+}
